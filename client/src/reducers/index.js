b972994e04b25587177e2d0afce93502f2c784c3
dylanOshima <dylanoshima@gmail.com>
2019-08-30 14:59:17 +0800

    Updates names of reducers in state

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import orderReducer from './OrderReducer';
 import dishReducer from './DishReducer';
 import serverReducer from './ServerReducer';
+import modalReducer from './ModalReducer';
 
 const rootReducer = combineReducers({
-  orderReducer,
-  dishReducer,
-  serverReducer
+  order: orderReducer,
+  dish: dishReducer,
+  server: serverReducer,
+  modal: modalReducer
 });
 
 export default rootReducer;
