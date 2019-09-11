6c0cb38c84c927510dcdb07ebfc772809f456be4
dylanOshima <dylanoshima@gmail.com>
2019-09-11 14:58:44 +0800

    Updates root route index

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,9 @@ const express   = require('express');
 const router    = express.Router();
 
 
-//get menu list from selected menu section
-// router.get('/', (req, res, next) => {
-//    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-// DEBUG
-router.get('/', (req, res) => {
-   console.log(req);
-   res.send("Wassup bitchez");
- });
+// get menu list from selected menu section
+router.get('/', (req, res, next) => {
+   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
