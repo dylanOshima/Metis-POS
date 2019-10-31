0730af518548de060e6fd21d88cf7258c902ebea
dylanOshima <dylanoshima@gmail.com>
2019-10-30 19:14:55 -0700

    Connects to remote DB by default

--- models/connection-string.js
+++ models/connection-string.js
@@ -1,8 +1,16 @@
 //Holds connection information to MongoDB
 
-var url = require('url')
+let mongoDB;
+try {
+  const config = require('../config.js');
+  mongoDB = config.mongoDB;
+} catch {
+  console.error("No config file located");
+}
+var url = require('url');
+var uri = mongoDB ? `mongodb+srv://${mongoDB.account}:${mongoDB.password}@metis-0-pjyqg.azure.mongodb.net/test?retryWrites=true&w=majority`
+:'mongodb://localhost:27017/restaurant';
 
-var uri = (process.env.MONGODB_URI || 'mongodb://localhost:27017/restaurant');
 if (!uri) {
   throw new Error(
     '\033[31mYou need to provide the connection string. ' +
@@ -11,8 +19,8 @@ if (!uri) {
 }
 
 //verifies the mongo url
-var uriObj = url.parse(uri)
-if (uriObj.protocol !== 'mongodb:') {
+var uriObj = url.parse(uri);
+if (!/^mongodb/.test(uriObj.protocol)) {
   throw new Error('Must be a mongodb URI')
 }
 if (!uriObj.host || !uriObj.path) {
